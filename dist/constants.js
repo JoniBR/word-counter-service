8ba62701ab7328ba2f3ebf4ee0fb9224b2ff6a2a
Yoni Braslaver <Yoni@zira.us>
2020-05-04 19:52:32 +0300

    delete dist

--- dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVICE_NAME = 'WORD_COUNTER';
-var SourceType;
-(function (SourceType) {
-    SourceType[SourceType["URL"] = 0] = "URL";
-    SourceType[SourceType["FILE"] = 1] = "FILE";
-})(SourceType = exports.SourceType || (exports.SourceType = {}));
